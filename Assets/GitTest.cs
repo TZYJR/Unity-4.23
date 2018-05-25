539ebed1e9f7300275bc281c98339ff37458ed83
YangJiaQi <1295585769@qq.com>
2018-05-25 17:44:28 +0800

    Test

---
+++ Assets/GitTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+
+    /// <summary>
+    /// 测试git尝试理解是手动上传还是自动更新
+    /// Player
+    /// </summary>
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
